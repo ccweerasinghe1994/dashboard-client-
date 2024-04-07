2a2b5a170e7e4a3e027479d4c949fa4d5c0c68c4
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-04-07 08:44:19 +0530

    adding server port

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    port: 3000,
+  },
   plugins: [react()],
   resolve: {
     alias: [
